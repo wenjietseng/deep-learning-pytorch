28daed452ba6ad7aa28343fc762e03e19bc2e19f
wenjietseng <wenjietseng@gmail.com>
2018-04-04 13:18:44 +0800

    update req 1

--- Lab2-DeepImagePrior/parametrization.py
+++ Lab2-DeepImagePrior/parametrization.py
@@ -19,39 +19,24 @@ torch.backends.cudnn.enabled = True
 torch.backends.cudnn.benchmark =True
 dtype = torch.cuda.FloatTensor
 
-imsize =-1
+imsize = -1
 PLOT = True
 sigma = 25
 sigma_ = sigma/255.
+fname = './images/brown-falcon.jpg'
 
-## deJPEG 
-fname = 'data/denoising/snail.jpg'
-
-## denoising
-# fname = 'data/denoising/F16_GT.png'
 # --- Load image ---
-if fname == 'data/denoising/snail.jpg':
-    img_noisy_pil = crop_image(get_image(fname, imsize)[0], d=32)
-    img_noisy_np = pil_to_np(img_noisy_pil)
+img_noisy_pil = crop_image(get_image(fname, imsize)[0], d=32)
+img_noisy_np = pil_to_np(img_noisy_pil)
     
-    # As we don't have ground truth
-    img_pil = img_noisy_pil
-    img_np = img_noisy_np
+# As we don't have ground truth
+img_pil = img_noisy_pil
+img_np = img_noisy_np
     
-    if PLOT:
-        plot_image_grid([img_np], 4, 5)
+if PLOT:
+    plot_image_grid([img_np], 4, 5)
         
-elif fname == 'data/denoising/F16_GT.png':
-    # Add synthetic noise
-    img_pil = crop_image(get_image(fname, imsize)[0], d=32)
-    img_np = pil_to_np(img_pil)
-    
-    img_noisy_pil, img_noisy_np = get_noisy_image(img_np, sigma_)
-    
-    if PLOT:
-        plot_image_grid([img_np, img_noisy_np], 4, 6)
-else:
-    assert False
+
 # --- Setup ---
 INPUT = 'noise' # 'meshgrid'
 pad = 'reflection'
@@ -62,36 +47,21 @@ LR = 0.01
 
 OPTIMIZER='adam' # 'LBFGS'
 show_every = 500
+num_iter=2400
+input_depth = 3
+figsize = 5 
 
-if fname == 'data/denoising/snail.jpg':
-    num_iter=2400
-    input_depth = 3
-    figsize = 5 
-    
-    net = skip(
-                input_depth, 3, 
-                num_channels_down = [8, 16, 32, 64, 128], 
-                num_channels_up   = [8, 16, 32, 64, 128],
-                num_channels_skip = [0, 0, 0, 4, 4], 
-                upsample_mode='bilinear',
-                need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU')
+net = skip(
+            input_depth, 3, 
+            num_channels_down = [8, 16, 32, 64, 128], 
+            num_channels_up   = [8, 16, 32, 64, 128],
+            num_channels_skip = [0, 0, 0, 4, 4], 
+            upsample_mode='bilinear',
+            need_sigmoid=True, need_bias=True, pad=pad, act_fun='LeakyReLU')
 
-    net = net.type(dtype)
+net = net.type(dtype)
 
-elif fname == 'data/denoising/F16_GT.png':
-    num_iter = 3000
-    input_depth = 32 
-    figsize = 4 
-    
-    net = get_net(input_depth, 'skip', pad,
-                  skip_n33d=128, 
-                  skip_n33u=128, 
-                  skip_n11=4, 
-                  num_scales=5,
-                  upsample_mode='bilinear').type(dtype)
 
-else:
-    assert False
     
 net_input = get_noise(input_depth, INPUT, (img_pil.size[1], img_pil.size[0])).type(dtype).detach()
 
@@ -125,6 +95,9 @@ def closure():
     if  PLOT and i % show_every == 0:
         out_np = var_to_np(out)
         plot_image_grid([np.clip(out_np, 0, 1)], factor=figsize, nrow=1)
+        plt.savefig("./out_imgs/"+ i + ".png")
+        plt.close()
+
         
     i += 1
 
