49845250b44810da7c9cd5f0aecb2113beefc84a
wenjietseng <wenjietseng@gmail.com>
2018-04-09 00:57:51 +0800

    6th req1

--- Lab2-DeepImagePrior/parametrization.py
+++ Lab2-DeepImagePrior/parametrization.py
@@ -109,7 +109,7 @@ img_noisy_var = np_to_var(img_noisy_np).type(dtype)
 net_input_saved = net_input.data.clone()
 noise = net_input.data.clone()
 
-training_loss_writer = csv.writer(open("./output/5th-out"+ para_setup +".csv", 'w'))
+training_loss_writer = csv.writer(open("./output/6th-out"+ para_setup +".csv", 'w'))
 
 i = 0
 def closure():
@@ -132,7 +132,7 @@ def closure():
     if  PLOT and i % show_every == 0:
         out_np = var_to_np(out)
         plot_image_grid([np.clip(out_np, 0, 1)], factor=figsize, nrow=1)
-        plt.savefig("./out_imgs/5th-setup" + para_setup + "-" + str(i) + ".png", bbox_inches="tight")
+        plt.savefig("./out_imgs/6th-setup" + para_setup + "-" + str(i) + ".png", bbox_inches="tight")
         plt.close()
 
     training_loss_writer.writerow([i, total_loss.data[0]])
@@ -150,5 +150,5 @@ out_np = var_to_np(net(net_input))
 print(type(np.clip(out_np,0,1)))
 print(type(img_np))
 q = plot_image_grid([np.clip(out_np, 0, 1), img_np], factor=13) 
-plt.savefig("./out_imgs/5th-setup" + para_setup + "-final-compare.png", bbox_inches="tight")
+plt.savefig("./out_imgs/6th-setup" + para_setup + "-final-compare.png", bbox_inches="tight")
 plt.close()
