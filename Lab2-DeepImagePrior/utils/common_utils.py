057fb961a824dde889480d2b2165e4d4043e6421
wenjietseng <wenjietseng@gmail.com>
2018-04-09 13:17:34 +0800

    crop LR

--- Lab2-DeepImagePrior/utils/common_utils.py
+++ Lab2-DeepImagePrior/utils/common_utils.py
@@ -255,14 +255,13 @@ def shuffled_img(img_path,BLOCKLEN=2):
 
 
 def crop_LR(LR_var_from_HR, d=32):
-    #tmp = var_to_np(LR_var_from_HR)
-    print(LR_var_from_HR.shape)
-    new_size = (tmp.shape[1] - tmp.shape[1] % d,
-                tmp.shape[2] - tmp.shape[2] % d)
+    #print(LR_var_from_HR.shape) 1, 3, 96, 144
+    new_size = (LR_var_from_HR.shape[2] - LR_var_from_HR.shape[2] % d,
+                LR_var_from_HR.shape[3] - LR_var_from_HR.shape[3] % d)
     bbox = [
-            int((tmp.shape[2] - new_size[0]) / 2),
-            int((tmp.shape[3] - new_size[1]) / 2),
-            int((tmp.shape[2] + new_size[0]) / 2),
-            int((tmp.shape[3] + new_size[1]) / 2),
+            int((LR_var_from_HR.shape[2] - new_size[0]) / 2),
+            int((LR_var_from_HR.shape[3] - new_size[1]) / 2),
+            int((LR_var_from_HR.shape[2] + new_size[0]) / 2),
+            int((LR_var_from_HR.shape[3] + new_size[1]) / 2),
     ]
-    return tmp[:, bbox[0]:bbox[2], bbox[1]:bbox[3]]
+    return LR_var_from_HR[:, :, bbox[0]:bbox[2], bbox[1]:bbox[3]]
