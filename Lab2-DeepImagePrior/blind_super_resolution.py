bc84813131fcfed35714c8cf39130e91222273d5
wenjietseng <wenjietseng@gmail.com>
2018-04-09 12:28:12 +0800

    sr

--- Lab2-DeepImagePrior/blind_super_resolution.py
+++ Lab2-DeepImagePrior/blind_super_resolution.py
@@ -1,7 +1,7 @@
 # --- Import libs ---
 from __future__ import print_function
 import matplotlib
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 #%matplotlib inline
 
@@ -32,25 +32,33 @@ PLOT = True
 
 # To produce images from the paper we took *_GT.png images from LapSRN viewer for corresponding factor,
 # e.g. x4/zebra_GT.png for factor=4, and x8/zebra_GT.png for factor=8 
-path_to_image = './images/SR_GT.png'
+LR_image_path = './images/LowResolution.png'
+GT_image_path = './images/SR_GT.png'
+
+# record result of each training
 time = sys.argv[1]
 if time == None:
     exit("enter something to record the name of this training!")
 
 # --- Load Image and Baselines ---
-# Starts here
-imgs = load_LR_HR_imgs_sr(path_to_image , imsize, factor, enforse_div32)
+img_LR_pil = crop_image(get_image(LR_image_path, imsize)[0], d=32)
+img_GT_pil = crop_image(get_image(GT_image_path, imsize)[0], d=32)
 
-imgs['bicubic_np'], imgs['sharp_np'], imgs['nearest_np'] = get_baselines(imgs['LR_pil'], imgs['HR_pil'])
+img_LR_np = pil_to_np(img_LR_pil)
+img_GT_np = pil_to_np(img_GT_pil)
 
-if PLOT:
-    plot_image_grid([imgs['HR_np'], imgs['bicubic_np'], imgs['sharp_np'], imgs['nearest_np']], 4,12)
-    print ('PSNR bicubic: %.4f   PSNR nearest: %.4f' %  (
-                                        compare_psnr(imgs['HR_np'], imgs['bicubic_np']), 
-                                        compare_psnr(imgs['HR_np'], imgs['nearest_np'])))
+# imgs = load_LR_HR_imgs_sr(path_to_image , imsize, factor, enforse_div32)
+# imgs['bicubic_np'], imgs['sharp_np'], imgs['nearest_np'] = get_baselines(imgs['LR_pil'], imgs['HR_pil'])
+# if PLOT:
+#     plot_image_grid([imgs['HR_np'], imgs['bicubic_np'], imgs['sharp_np'], imgs['nearest_np']], 4,12)
+#     print ('PSNR bicubic: %.4f   PSNR nearest: %.4f' %  (
+#                                         compare_psnr(imgs['HR_np'], imgs['bicubic_np']), 
+#                                         compare_psnr(imgs['HR_np'], imgs['nearest_np'])))
 
-# for key, fig in imgs.items():
-    # print(key, fig.size)
+plot_image_grid([img_LR_np], factor=13)
+plot_image_grid([img_GT_np], factor=13)
+
+
 # --- Set up parameters and net --
 input_depth = 32
  
@@ -73,8 +81,7 @@ elif factor == 8:
 else:
     assert False, 'We did not experiment with other factors'
 
-net_input = get_noise(input_depth, INPUT, (imgs['HR_pil'].size[1], imgs['HR_pil'].size[0])).type(dtype).detach()
-# print(net_input)
+net_input = get_noise(input_depth, INPUT, (img_GT_pil.size[1], img_GT_pil.size[0])).type(dtype).detach()
 NET_TYPE = 'skip' # UNet, ResNet
 net = get_net(input_depth, 'skip', pad,
               skip_n33d=128, 
@@ -86,10 +93,9 @@ net = get_net(input_depth, 'skip', pad,
 # Losses
 mse = torch.nn.MSELoss().type(dtype)
 
-img_LR_var = np_to_var(imgs['LR_np']).type(dtype)
-# print(img_LR_var)
+img_LR_var = np_to_var(img_LR_np).type(dtype)
 downsampler = Downsampler(n_planes=3, factor=factor, kernel_type=KERNEL_TYPE, phase=0.5, preserve_size=True).type(dtype)
-# print(downsampler)
+
 # --- Define closure and optimize ---
 def closure():
     global i
@@ -97,11 +103,8 @@ def closure():
     if reg_noise_std > 0:
         net_input.data = net_input_saved + (noise.normal_() * reg_noise_std)
     
-    # an interesting part
     out_HR = net(net_input)
-    # print(out_HR)
     out_LR = downsampler(out_HR)
-    # print(out_LR)
     # use the mse of downsampled img of out_HR to do backpropagation
     total_loss = mse(out_LR, img_LR_var) 
     
@@ -111,8 +114,8 @@ def closure():
     total_loss.backward()
 
     # Log
-    psnr_LR = compare_psnr(imgs['LR_np'], var_to_np(out_LR))
-    psnr_HR = compare_psnr(imgs['HR_np'], var_to_np(out_HR))
+    psnr_LR = compare_psnr(img_LR_np, var_to_np(out_LR))
+    psnr_HR = compare_psnr(img_GT_np, var_to_np(out_HR))
     print ('Iteration %05d    PSNR_LR %.3f   PSNR_HR %.3f' % (i, psnr_LR, psnr_HR), '\r', end='')
                       
     # History
@@ -121,7 +124,7 @@ def closure():
 
     if PLOT and i % 500 == 0:
         out_HR_np = var_to_np(out_HR)
-        plot_image_grid([imgs['HR_np'], imgs['bicubic_np'], np.clip(out_HR_np, 0, 1)], factor=13, nrow=3)
+        plot_image_grid([np.clip(out_HR_np, 0, 1)], factor=13, nrow=3)
         plt.savefig("./out_imgs/req3-" + time + "-" + str(i) + ".png", bbox_inches="tight")
         plt.close()
     i += 1
@@ -138,17 +141,11 @@ p = get_params(OPT_OVER, net, net_input)
 optimize(OPTIMIZER, p, closure, LR, num_iter)
 
 out_HR_np = np.clip(var_to_np(net(net_input)), 0, 1)
-result_deep_prior = put_in_center(out_HR_np, imgs['orig_np'].shape[1:])
+# result_deep_prior = put_in_center(out_HR_np, imgs['orig_np'].shape[1:])
 
 # For the paper we actually took `_bicubic.png` files from LapSRN viewer and used `result_deep_prior` as our result
-plot_image_grid([imgs['HR_np'],
-                 imgs['bicubic_np'],
+plot_image_grid([img_GT_np,
                  out_HR_np], factor=4, nrow=1)
 
 plt.savefig("./out_imgs/req3-final" + time + "-" + str(i) + ".png", bbox_inches="tight")
-plt.close()
-
-
-plot_image_grid([result_deep_prior], factor=13)
-plt.savefig("./out_imgs/req3-resultDeepPrior-" + time + "-" + str(i) + ".png", bbox_inches="tight")
 plt.close()
