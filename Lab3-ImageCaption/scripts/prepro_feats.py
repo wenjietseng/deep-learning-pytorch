3219d6238bd9ebf9ac132d321dceaef565781a16
wenjietseng <wenjietseng@gmail.com>
2018-04-23 01:21:07 +0800

    misc

--- Lab3-ImageCaption/scripts/prepro_feats.py
+++ Lab3-ImageCaption/scripts/prepro_feats.py
@@ -44,7 +44,7 @@ preprocess = trn.Compose([
         trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
-from ..misc import *
+from misc.resnet_utils import myResnet 
 import misc.resnet as resnet
 
 
