8781e39068a0b2073b504a2a1720775931cd3435
wenjietseng <wenjietseng@gmail.com>
2018-04-23 20:19:32 +0800

    add run.sh

---
+++ Lab3-ImageCaption/run.sh
@@ -0,0 +1,5 @@
+python train.py --id st --caption_model show_attend_tell --input_json data/cocotalk.json \
+--input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
+--input_label_h5 data/cocotalk_label.h5 --batch_size 10 --learning_rate 5e-4 \
+--learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_st \
+--save_checkpoint_every 6000 --val_images_use 5000 --max_epochs 5
