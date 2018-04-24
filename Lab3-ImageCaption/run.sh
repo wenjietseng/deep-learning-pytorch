1859240477e0179fa2d1ec5e1252f8cc0ee582c7
wenjietseng <wenjietseng@gmail.com>
2018-04-24 14:44:48 +0800

    eval shell

--- Lab3-ImageCaption/run.sh
+++ Lab3-ImageCaption/run.sh
@@ -1,8 +1,14 @@
-python train.py --id showAttendTell --caption_model show_attend_tell --input_json data/cocotalk.json \
---input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
---input_label_h5 data/cocotalk_label.h5 --batch_size 10 --learning_rate 5e-4 \
---learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_showAttendTell \
---save_checkpoint_every 6000 --val_images_use 5000 --max_epochs 5
+# python train.py --id showAttendTell --caption_model show_attend_tell --input_json data/cocotalk.json \
+# --input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
+# --input_label_h5 data/cocotalk_label.h5 --batch_size 10 --learning_rate 5e-4 \
+# --learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_showAttendTell \
+# --save_checkpoint_every 6000 --val_images_use 5000 --max_epochs 5
+
+# eval show attend tell
+echo "eval show attend tell"
+python2 eval.py --model log_showAttendTell/model-best.pth --info_path log_showAttendTell/ \
+--image_folder interest/ --num_images 50
+
 
 # python train.py --id topDown --caption_model topdown --input_json data/cocotalk.json \
 # --input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
