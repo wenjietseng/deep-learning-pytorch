aae7701a8016a710511ef0d360225803b4d91417
wenjietseng <wenjietseng@gmail.com>
2018-05-04 22:12:39 +0800

    run.sh

--- Lab3-ImageCaption/run.sh
+++ Lab3-ImageCaption/run.sh
@@ -20,7 +20,7 @@ echo "karpathy's test split"
 python2 eval.py --num_images 50 \
 --model ./log_showAttendTell/model-best.pth \
 --infos_path ./log_showAttendTell/infos_showAttendTell-best.pkl \
---language_eval 1
+--language_eval 1 --image_root ~/coco14
 
 #
 # topdown
