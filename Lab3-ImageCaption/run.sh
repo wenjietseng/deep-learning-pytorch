1d319a0b4b3a5a90d329834a2f779a46ed66709b
wenjietseng <wenjietseng@gmail.com>
2018-05-04 21:57:58 +0800

    modified run

--- Lab3-ImageCaption/run.sh
+++ Lab3-ImageCaption/run.sh
@@ -16,15 +16,19 @@
 # --image_folder ./interest --num_images 50
 
 # eval karpathy's test split
-# echo "karpathy's test split"
-# python2 eval.py --num_images 2 \
-# --model ./log_showAttendTell/model-best.pth \
-# --infos_path ./log_showAttendTell/infos_showAttendTell-best.pkl \
-# --language_eval 1 --image_folder ~/coco14/val2014
+echo "karpathy's test split"
+python2 eval.py --num_images 2 \
+--model ./log_showAttendTell/model-best.pth \
+--infos_path ./log_showAttendTell/infos_showAttendTell-best.pkl \
+--language_eval 1
 
-python train.py --id topDown --caption_model topdown --input_json data/cocotalk.json \
---input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
---input_label_h5 data/cocotalk_label.h5 --batch_size 10 --learning_rate 5e-4 \
---learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_topDown \
---save_checkpoint_every 6000 --val_images_use 5000 --max_epochs 5
+#
+# topdown
+#
+
+# python train.py --id topDown --caption_model topdown --input_json data/cocotalk.json \
+# --input_fc_dir data/cocotalk_fc --input_att_dir data/cocotalk_att \
+# --input_label_h5 data/cocotalk_label.h5 --batch_size 10 --learning_rate 5e-4 \
+# --learning_rate_decay_start 0 --scheduled_sampling_start 0 --checkpoint_path log_topDown \
+# --save_checkpoint_every 6000 --val_images_use 5000 --max_epochs 5
 
