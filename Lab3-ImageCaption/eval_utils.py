410c061a4e7f09ae0c01ec25f17e81fbb372f1b4
wenjietseng <wenjietseng@gmail.com>
2018-05-05 16:15:24 +0800

    add

--- Lab3-ImageCaption/eval_utils.py
+++ Lab3-ImageCaption/eval_utils.py
@@ -131,7 +131,7 @@ def eval_split(model, crit, loader, eval_kwargs={}):
                 plt.subplot(4, 5, 1)
                 plt.imshow(origin_img)
                 plt.axis('off')
-                alphas = weights[k]
+                alphas = weights#[k]
                 # print(alphas)
                 for t in range(len(words)):
                     if t > 18:
@@ -139,7 +139,7 @@ def eval_split(model, crit, loader, eval_kwargs={}):
                     plt.subplot(4, 5, t + 2)
                     plt.text(0, 1, '%s' % (words[t]), color='black', backgroundcolor='white', fontsize=8)
                     plt.imshow(origin_img)
-                    alpha = alphas[t]
+                    alpha = alphas#[t]
                     index = Variable(torch.cuda.LongTensor([k*loader.seq_per_img]))
                     alpha = torch.index_select(alpha, 0, index)
                     print(alpha)
