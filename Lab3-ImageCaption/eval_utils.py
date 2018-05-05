3585723c3eeea1c6fe41431b903c6c0d0b9ec18d
wenjietseng <wenjietseng@gmail.com>
2018-05-05 15:10:35 +0800

    att

--- Lab3-ImageCaption/eval_utils.py
+++ Lab3-ImageCaption/eval_utils.py
@@ -19,9 +19,7 @@ import misc.utils as utils
 from PIL import Image
 import skimage.transform
 from matplotlib import pyplot as plt
-import matplotlib
-matplotlib.use('Agg')
-
+plt.ioff()
 
 def language_eval(dataset, preds, model_id, split):
     import sys
@@ -131,7 +129,7 @@ def eval_split(model, crit, loader, eval_kwargs={}):
                 words = entry['caption'].split(' ')
                 origin_img = Image.open('vis/imgs/img' + str(len(predictions)) + '.jpg')
                 print("finish load")
-                plt.clf()
+                # plt.clf()
                 plt.subplot(4, 5, 1)
                 plt.imshow(origin_img)
                 plt.axis('off')
