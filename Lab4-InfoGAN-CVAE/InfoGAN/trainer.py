0456541c63e49d8f1243b3194fb2b7a7742add82
wenjietseng <wenjietseng@gmail.com>
2018-05-11 11:57:47 +0800

    check size

--- Lab4-InfoGAN-CVAE/InfoGAN/trainer.py
+++ Lab4-InfoGAN-CVAE/InfoGAN/trainer.py
@@ -94,79 +94,89 @@ class Trainer:
     print(fix_noise.size())
 
     for epoch in range(80):
-      for num_iters, batch_data in enumerate(dataloader, 0):
-        ############################
-        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
-        ###########################
-        # train with real part
-        optimD.zero_grad()
-        
-        x, _ = batch_data
+        for num_iters, batch_data in enumerate(dataloader, 0):
+            ############################
+            # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+            ###########################
+            # train with real part
+            optimD.zero_grad()
+            
+            x, _ = batch_data
+            print(x.size())
 
-        bs = x.size(0)
-        real_x.data.resize_(x.size())
-        label.data.resize_(bs)
-        dis_c.data.resize_(bs, 10)
-        # con_c.data.resize_(bs, 2)
-        noise.data.resize_(bs, 54)
-        
-        real_x.data.copy_(x)
-        fe_out1 = self.FE(real_x)
-        probs_real = self.D(fe_out1)
-        label.data.fill_(1)
-        loss_real = criterionD(probs_real, label)
-        loss_real.backward()
 
-        # train with fake part
-        z, idx = self._noise_sample(dis_c, noise, bs) # remove con_c
-        fake_x = self.G(z)
-        fe_out2 = self.FE(fake_x.detach())
-        probs_fake = self.D(fe_out2)
-        label.data.fill_(0)
-        loss_fake = criterionD(probs_fake, label)
-        loss_fake.backward()
+            bs = x.size(0)
+            print(bs)
+            real_x.data.resize_(x.size())
+            print(real_x.size())
+            label.data.resize_(bs)
+            print(label.size())
+            dis_c.data.resize_(bs, 10)
+            print(dis_c.size())
+            # con_c.data.resize_(bs, 2)
+            noise.data.resize_(bs, 54)
+            print(noise.size())
 
-        D_loss = loss_real + loss_fake
 
-        optimD.step()
-        ############################
-        # (2) Update G network: maximize log(D(G(z)))
-        ###########################        
-        # G and Q part
-        optimG.zero_grad()
 
-        fe_out = self.FE(fake_x)
-        probs_fake = self.D(fe_out)
-        label.data.fill_(1.0)
 
-        reconstruct_loss = criterionD(probs_fake, label)
-        
-        q_logits, q_mu, q_var = self.Q(fe_out)
-        class_ = torch.LongTensor(idx).cuda()
-        target = Variable(class_)
-        dis_loss = criterionQ_dis(q_logits, target)
-        # con_loss = criterionQ_con(con_c, q_mu, q_var)*0.1
-        
-        G_loss = reconstruct_loss + dis_loss #+ con_loss
-        G_loss.backward()
-        optimG.step()
+            real_x.data.copy_(x)
+            fe_out1 = self.FE(real_x)
+            probs_real = self.D(fe_out1)
+            label.data.fill_(1)
+            loss_real = criterionD(probs_real, label)
+            loss_real.backward()
 
-        if num_iters % 100 == 0:
+            # train with fake part
+            z, idx = self._noise_sample(dis_c, noise, bs) # remove con_c
+            fake_x = self.G(z)
+            fe_out2 = self.FE(fake_x.detach())
+            probs_fake = self.D(fe_out2)
+            label.data.fill_(0)
+            loss_fake = criterionD(probs_fake, label)
+            loss_fake.backward()
 
-          print('Epoch/Iter:{0}/{1}, Dloss: {2}, Gloss: {3}'.format(
-            epoch, num_iters, D_loss.data.cpu().numpy(),
-            G_loss.data.cpu().numpy())
-          )
+            D_loss = loss_real + loss_fake
 
-          noise.data.copy_(fix_noise)
-          dis_c.data.copy_(torch.Tensor(one_hot))
+            optimD.step()
+            ############################
+            # (2) Update G network: maximize log(D(G(z)))
+            ###########################        
+            # G and Q part
+            optimG.zero_grad()
 
-        #   con_c.data.copy_(torch.from_numpy(c1))
-          z = torch.cat([noise, dis_c], 1).view(-1, 64, 1, 1) # remove con_c after dis_c
-          x_save = self.G(z)
-          save_image(x_save.data, './tmp/c1.png', nrow=10)
+            fe_out = self.FE(fake_x)
+            probs_fake = self.D(fe_out)
+            label.data.fill_(1.0)
 
-        #   con_c.data.copy_(torch.from_numpy(c2))
-          z = torch.cat([noise, dis_c], 1).view(-1, 64, 1, 1) # remove con_c after dis_c
-          x_save = self.G(z)
-          save_image(x_save.data, './tmp/c2.png', nrow=10)
+            reconstruct_loss = criterionD(probs_fake, label)
+            
+            q_logits, q_mu, q_var = self.Q(fe_out)
+            class_ = torch.LongTensor(idx).cuda()
+            target = Variable(class_)
+            dis_loss = criterionQ_dis(q_logits, target)
+            # con_loss = criterionQ_con(con_c, q_mu, q_var)*0.1
+            
+            G_loss = reconstruct_loss + dis_loss #+ con_loss
+            G_loss.backward()
+            optimG.step()
+
+            if num_iters % 100 == 0:
+
+            print('Epoch/Iter:{0}/{1}, Dloss: {2}, Gloss: {3}'.format(
+                epoch, num_iters, D_loss.data.cpu().numpy(),
+                G_loss.data.cpu().numpy())
+            )
+
+            noise.data.copy_(fix_noise)
+            dis_c.data.copy_(torch.Tensor(one_hot))
+
+            #   con_c.data.copy_(torch.from_numpy(c1))
+            z = torch.cat([noise, dis_c], 1).view(-1, 64, 1, 1) # remove con_c after dis_c
+            x_save = self.G(z)
+            save_image(x_save.data, './tmp/c1.png', nrow=10)
+
+            #   con_c.data.copy_(torch.from_numpy(c2))
+            z = torch.cat([noise, dis_c], 1).view(-1, 64, 1, 1) # remove con_c after dis_c
+            x_save = self.G(z)
+            save_image(x_save.data, './tmp/c2.png', nrow=10)
