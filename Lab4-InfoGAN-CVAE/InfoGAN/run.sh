0e92960a08a9a82fac03e1f7b651c3eb6128a03e
wenjietseng <wenjietseng@gmail.com>
2018-05-13 01:18:53 +0800

    typos

--- Lab4-InfoGAN-CVAE/InfoGAN/run.sh
+++ Lab4-InfoGAN-CVAE/InfoGAN/run.sh
@@ -1,4 +1,4 @@
 python3 main.py --dataset MNIST --dataroot ~/Data \
---netG ./saved_models --netD ./saved_models \
+# --netG ./saved_models --netD ./saved_models \
 --outf ./out_figs \
 --manualSeed 719 --cuda
