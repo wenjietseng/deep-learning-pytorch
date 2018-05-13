c66ee0eaee526b967aede60e5073affd4b96d823
wenjietseng <wenjietseng@gmail.com>
2018-05-13 15:36:57 +0800

    variable vs tensor, _noise

--- Lab4-InfoGAN-CVAE/InfoGAN/main.py
+++ Lab4-InfoGAN-CVAE/InfoGAN/main.py
@@ -161,7 +161,7 @@ class Discriminator(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
-            nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
+            nn.Conv2d(1, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
             nn.Conv2d(ndf, ndf*2, 4, 2, 1, bias=False),
@@ -191,7 +191,7 @@ class Discriminator(nn.Module):
         if input.is_cuda and self.ngpu > 1:
             output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
         else:
-            print(input.size())
+            # print(input.size())
             output = self.main(input)
             # print(output.view(64, -1, 1, 1).size())
             # error resize outpur of discriminator to 64 x 8192 for Linear layer
