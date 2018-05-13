6380c5c846951cbbcc23286be6cf8c1adac9bd68
wenjietseng <wenjietseng@gmail.com>
2018-05-13 20:49:27 +0800

    eval

--- Lab4-InfoGAN-CVAE/InfoGAN/eval.py
+++ Lab4-InfoGAN-CVAE/InfoGAN/eval.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import torch
+import torch.nn as nn
+import torch.nn.parallel
 import torch.backends.cudnn as cudnn
 import torchvision.utils as vutils
 from matplotlib import pyplot as plt
