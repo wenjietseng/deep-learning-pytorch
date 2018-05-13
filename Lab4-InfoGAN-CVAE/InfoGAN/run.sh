3876cbe5c27d06036ae0ab66881b2b5a90050184
wenjietseng <wenjietseng@gmail.com>
2018-05-13 20:46:32 +0800

    run.sh

--- Lab4-InfoGAN-CVAE/InfoGAN/run.sh
+++ Lab4-InfoGAN-CVAE/InfoGAN/run.sh
@@ -1,4 +1,4 @@
 # python3 main.py --dataset MNIST --dataroot ~/Data \
 # --outf ./out_figs --cuda
 
-python3 eval.py --dataset MNIST --dataroot . --model ./out_figs/netG_final.pth --cuda
+python3 eval.py --model ./out_figs/netG_final.pth --cuda
