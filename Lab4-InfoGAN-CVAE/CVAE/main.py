859d1010291f96a18f906dfcb07d79aa6908bae5
wenjietseng <wenjietseng@gmail.com>
2018-05-14 00:00:49 +0800

    test unsqueeze

--- Lab4-InfoGAN-CVAE/CVAE/main.py
+++ Lab4-InfoGAN-CVAE/CVAE/main.py
@@ -151,14 +151,15 @@ def train(epoch):
         # one_hot_tensor = one_hot_tensor.expand(-1, -1, 28, 28) # batch_size x 10 x 28 x 28
         # print(one_hot_tensor.size())
         # new_data = torch.cat((data, one_hot_tensor), dim=1)
-        one_hot_tensor = torch.Tensor(one_hot_lst).unsqueeze(-1)
-        print(one_hot_tensor.size())
+        one_hot_tensor = torch.Tensor(one_hot_lst).unsqueeze(-1).cuda()
+        # print(one_hot_tensor.size())
         one_hot_tensor = torch.unsqueeze(one_hot_tensor, -1)
-        print(one_hot_tensor.size())
+        # print(one_hot_tensor.size())
         one_hot_tensor = one_hot_tensor.expand(-1, -1, 28, 28)
-        print(one_hot_tensor.size())
+        # print(one_hot_tensor.size())
+        new_data = torch.cat((data, one_hot_tensor), dim=1)
 
-        # print(new_data.size())
+        print(new_data.size())
         # print(new_data.size()) 128 x 11 x 28 x 28
 
         optimizer.zero_grad()
