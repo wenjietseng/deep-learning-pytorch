5a6972c32eaa4d780051b49f308e29424e9069d8
wenjietseng <wenjietseng@gmail.com>
2018-05-14 11:12:51 +0800

    add weight init for Linear

--- Lab4-InfoGAN-CVAE/InfoGAN/models.py
+++ Lab4-InfoGAN-CVAE/InfoGAN/models.py
@@ -9,7 +9,7 @@ def weights_init(m):
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
-    elif classname.fine('Linear') != -1:
+    elif classname.find('Linear') != -1:
         m.weight.data.normal(0.0, 0.02)
 
 class Generator(nn.Module):
